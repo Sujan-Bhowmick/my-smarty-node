311638412e9b65dbe9d15dd1af865c3885dd6119
Your Name <imsujan93@gmail.com>
2022-07-19 11:08:03 +0600

    added steps

--- index.js
+++ index.js
@@ -20,8 +20,12 @@ const users = [
     {id: 7, name: 'Sohana', email: 'sohana@gmail.com', phone: '01751235467'}
 ]
 
+// app.get('/users', (req, res) => {
+//     res.send(users)
+// })
+
+ // filter by search query parameter
 app.get('/users', (req, res) => {
-    // filter by search query parameter
     if(req.query.name){
         const search = req.query.name.toLocaleLowerCase();
         const matched = users.filter(user => user.name.toLocaleLowerCase().includes(search))
